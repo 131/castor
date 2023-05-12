bf5c31b8e417a27dd87307707d6f9452080c32c6
Florent Kolosowski <fkolosowski@ivsweb.com>
2023-05-12 16:03:46 +0200

    with chunks download feature

--- store.js
+++ store.js
@@ -1,9 +1,9 @@
 'use strict';
 
-const path = require('path');
-const fs   = require('fs');
-const crypto  = require('crypto');
-
+const crypto = require('crypto');
+const fs     = require('fs');
+const path   = require('path');
+const url    = require('url');
 
 const get               = require('mout/object/get');
 const set               = require('mout/object/set');
@@ -17,7 +17,8 @@ const guid         = require('mout/random/randString');
 const createWriteStream  = require('nyks/fs/createWriteStream');
 const rename       = require('nyks/fs/rename');
 const pipe         = require('nyks/stream/pipe');
-const fetch        = require('nyks/http/fetch');
+const request      = require('nyks/http/request');
+const defer        = require('nyks/promise/defer');
 
 
 const readdir    = require('nyks/fs/readdir');
@@ -127,7 +128,6 @@ class Store {
 
 
   async download(file_url, file_md5) {
-
     var file_path = this.getFilePathFromMd5(file_md5);
 
     try {
@@ -139,32 +139,60 @@ class Store {
     } catch(err) {}
 
     mkdirpSync(path.dirname(file_path));
-    var tmp_path    = `${file_path}.tmp.${guid()}`;
 
-    try {
-      var res = await fetch(file_url);
+    var tmp_path     = `${file_path}.tmp.${guid()}`;
+    var current_size = 0;
+    var hash         = crypto.createHash('md5');
 
-      if(!(res.statusCode >= 200 && res.statusCode < 300))
-        throw `Invalid status code '${res.statusCode}'`;
+    hash.setEncoding('hex');
 
-      const fd = fs.openSync(tmp_path, 'w+');
-      var outstream  = await createWriteStream(tmp_path, {fd});
-      var hash = crypto.createHash('md5');
-      hash.setEncoding('hex');
+    while(true) {
+      try {
+        file_url = url.parse(file_url);
+        file_url.headers = {
+          'Range' : `bytes=${current_size}-`
+        };
 
-      await Promise.all([pipe(res, hash), pipe(res, outstream)]);
-      await new Promise(resolve => fs.fsync(fd, resolve));
+        var res           = await request(file_url);
+        var expected_size = res.headers['content-length'];
+        var acceptRange   = !!res.headers['accept-ranges']; // Accept-Ranges: bytes ?
 
-      const challenge_md5    = hash.read();
-      if(challenge_md5 != file_md5)
-        throw `Corrupted file ${challenge_md5} != ${file_md5}`;
+        var defered = defer();
+        res.on('end', defered.resolve);
 
-      await rename(tmp_path, file_path);
-      return true;
-    } catch(err) {
-      if(fs.existsSync(tmp_path))
-        fs.unlinkSync(tmp_path);
-      throw err;
+        if(!(res.statusCode >= 200 && res.statusCode < 300))
+          throw `Invalid status code '${res.statusCode}'`;
+
+        const fd      = fs.openSync(tmp_path, 'a+');
+        var outstream = await createWriteStream(tmp_path, {fd});
+
+        pipe(res, hash, {end : false});
+        await pipe(res, outstream);
+
+        await new Promise(resolve => fs.fsync(fd, resolve));
+
+        let {size} = fs.statSync(tmp_path);
+
+        current_size = size;
+
+        if(!acceptRange || current_size >= expected_size) {
+          await defered;
+          hash.end();
+
+          const challenge_md5 = hash.read();
+
+          if(challenge_md5 != file_md5)
+            throw `Corrupted file ${challenge_md5} != ${file_md5}`;
+
+          await rename(tmp_path, file_path);
+          return true;
+        }
+
+      } catch(err) {
+        if(fs.existsSync(tmp_path))
+          fs.unlinkSync(tmp_path);
+        throw err;
+      }
     }
   }
 
--- test/index.js
+++ test/index.js
@@ -467,6 +467,51 @@ describe("Test Index Class", function() {
       expect(res.statusCode).to.eql(404);
       server.close();
     });
+
+
+    it("try to get file from partial request", async () => {
+      data            = guid(40);//global
+
+      const file_path = guid(10);
+      const file_md5  = md5(data);
+
+      var defered = defer();
+
+      fs.writeFileSync(file_path, data);
+      console.log('FILE CONTENT MIGHT BE : ', data, 'IN FILENAME', file_path, 'AND MD5', file_md5);
+
+      var server = http.createServer((req, res) => {
+        let {size : file_size} = fs.statSync(file_path);
+        let data_bytes          = Array.from(Buffer.from(data, 'utf8'));
+
+        res.setHeader("content-length", file_size); // keep real content-length here
+        res.setHeader("content-md5", file_md5); // why not
+
+        let [, , range] = new RegExp('^([a-zA-Z]+)=([0-9]+)-').exec(req.headers.range || 'bytes=5-');
+
+        range = parseInt(range);
+        res.end(Buffer.from(data_bytes.slice(range, range + 5))); // only send 5 bytes at a time
+      });
+
+      server.listen(0, function() {
+        defered.resolve(this.address().port);
+      });
+
+
+      var port         = await defered;
+      var target       = `http://127.0.0.1:${port}/${file_path}`;
+
+      var file      = new Store(index_path);
+      var ns        = guid(4);
+      var index     = file.getIndex(ns);
+      var touched   = await index.checkFile(file_path, target, file_md5);
+
+      fs.unlinkSync(file_path);
+
+      expect(touched).to.be(1);
+
+      server.close();
+    });
   });
 
 });
